8fd3c1dafbb2329ad680ec9240dc80a5241d0f18
Claire Lesh <cteterita@gmail.com>
2020-09-12 15:02:58 -0700

    added mock authorization checking

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const MOVIEDEX = require('./movies-data-small.json')
 
 const app = express();
 
@@ -9,8 +11,20 @@ app.use(morgan('common'));
 app.use(helmet());
 app.use(cors());
 
-app.use((req, res) => {
-    res.send('Hello, world!')
+app.use((req, res, next) => {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        res.send(401);
+    }
+    next();
 });
+
+function handleGetMovies(req, res) {
+    let results = MOVIEDEX;
+    return res.send(200, results);
+}
+
+app.get('/movies', handleGetMovies);
 
 module.exports = app;
